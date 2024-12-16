60e6ab4f03924be411bd8d8a505ec0401e325369
Prog-Memito <sa3776530@gmail.com>
2024-12-15 22:36:13 -0300

    Db conectada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,19 @@
 export const environment = {
   production: true,
-  firebase : {
-    apiKey: "AIzaSyBeg_S7evIzkxdrsG97RvsstzD8-nqUDHU",
-    authDomain: "app-asistencias-bf732.firebaseapp.com",
-    projectId: "app-asistencias-bf732",
-    storageBucket: "app-asistencias-bf732.appspot.com",
-    messagingSenderId: "241384130086",
-    appId: "1:241384130086:web:47e81c412fb3af8eeb48e7",
-    measurementId: "G-N6VT5VRD6W"
+  firebase: {
+    apiKey: "AIzaSyBVDJbTZFi-NPp1F8ig65hLSSGzCS8xu3M",
+
+    authDomain: "app-registro-duoc.firebaseapp.com",
+
+    projectId: "app-registro-duoc",
+
+    storageBucket: "app-registro-duoc.firebasestorage.app",
+
+    messagingSenderId: "296001699219",
+
+    appId: "1:296001699219:web:2c7cf6d44a555603707c66",
+
+    measurementId: "G-W5V592217T"
+
   }
 };
